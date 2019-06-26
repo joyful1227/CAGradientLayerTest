02cb8bb9a2d9ef7fc32d2a35e2a1c127d6d04a34
joy <le821227@gmail.com>
2019-06-26 23:39:05 +0800

    version1.0

--- CAGradientLayerTest/ViewController.swift
+++ CAGradientLayerTest/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var myView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        //建立CAGradientLayer 漸層物件
+        let gradientLayer = CAGradientLayer()
+        
+        
+        gradientLayer.colors = [UIColor.yellow.cgColor,UIColor.orange.cgColor,UIColor.brown.cgColor]
+        //gradientLayer.colors = [UIColor.yellow.cgColor,UIColor.orange.cgColor]
+        
+        //設置每個顏色漸變的點0.0~1.0
+        gradientLayer.locations = [0, 0.3, 0.6]
+        //gradientLayer.locations = [0,0.5]
+       
+        //漸層起始和結束位置
+        gradientLayer.startPoint = CGPoint(x:0, y:0)
+        gradientLayer.endPoint = CGPoint(x:1, y:1)
+        
+        gradientLayer.frame = myView.bounds
+        
+        
+        myView.layer.addSublayer(gradientLayer)
     }
 
 
